310394477f9fb3a3ae1e1461953c0f4bf4bd5651
Sebastian Friedrich <sebastian@feldpost.com>
2009-08-25 13:56:23 -0500

    Additional Parameters for PayflowPro Gateway

--- lib/active_merchant/billing/gateways/payflow.rb
+++ lib/active_merchant/billing/gateways/payflow.rb
@@ -105,12 +105,17 @@ module ActiveMerchant #:nodoc:
               xml.tag! 'CustIP', options[:ip] unless options[:ip].blank?
               xml.tag! 'InvNum', options[:order_id].to_s.gsub(/[^\w.]/, '') unless options[:order_id].blank?
               xml.tag! 'Description', options[:description] unless options[:description].blank?
+              xml.tag! 'MerchantDescription', options[:merchant] unless options[:merchant].blank?
+              xml.tag! 'Comment', options[:comment] unless options[:comment].blank?
+              xml.tag! 'ExtData', options[:comment2] unless options[:comment2].blank?
+              xml.tag! 'CustCode', options[:customer] unless options[:customer].blank?
 
               billing_address = options[:billing_address] || options[:address]
               add_address(xml, 'BillTo', billing_address, options) if billing_address
               add_address(xml, 'ShipTo', options[:shipping_address], options) if options[:shipping_address]
               
               xml.tag! 'TotalAmt', amount(money), 'Currency' => options[:currency] || currency(money)
+              xml.tag! 'TaxAmt', options[:tax] unless options[:tax].blank?
             end
             
             xml.tag! 'Tender' do
